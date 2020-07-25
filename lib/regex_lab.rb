2b06428e266584b8ec5adfbf7a07406a41216814
Keenah Vancampenhout <kvancamp@mail.com>
2020-07-25 04:17:04 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -17,7 +17,6 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-!!phone.match(/^(1?(-?\d{3})-?)?(\d{3})(-?\d{4})$/) ? true : false
-
+!!phone.scan(/(([01][\.\- +]\(\d{3}\)[\.\- +]?)|([01][\.\- +]\d{3}[\.\- +])|(\(\d{3}\) ?)|(\d{3}[- \.]))?\d{3}[- \.]\d{4}/) ? true : false
 
 end
