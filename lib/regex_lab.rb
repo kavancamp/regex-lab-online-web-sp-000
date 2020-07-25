b9bd5b5fc052f555046281d27272d7b4f9abd38a
Keenah Vancampenhout <kvancamp@mail.com>
2020-07-25 04:24:30 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -17,6 +17,6 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-!!phone.scan(/^\(?082|083|084|072\)?[\s-]?[\d]{3}[\s-]?[\d]{4}$/) ? true : false
-
+  phone.match?/\(?\d{3}\)?(\s|-)?\d{3}(\s|-)?\d{4}/
 end
+
